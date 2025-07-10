d95aa9f5a820aa11e0b4314d5b7413b6b05cd21b
Herr-Kokosnuss <ahmed.alsaeedi7890@gmail.com>
2025-07-10 20:18:21 +0200

    adding chrome to railway

--- scrapers/fly4free.js
+++ scrapers/fly4free.js
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
         '--disable-extensions'
       ],
       customConfig: {
-        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || process.env.CHROME_PATH || process.env.GOOGLE_CHROME_BIN || '/usr/bin/chromium'
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || 'chromium'
       },
       turnstile: true,
       connectOption: {},
